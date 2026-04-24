f2d3f92ab799585517cb38483568ff3152d892eb
Himal Bhattarai <himalbhattarai439@gmail.com>
2026-04-24 13:57:04 +0545

    day 3

---
+++ if_else.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int mark = 75;
+    if(mark>=80){
+        printf("A\n");
+    }else if(mark>=60){
+        printf("B\n");
+    }else{
+        printf("C");
+    }
+
+    return 0;
+}
