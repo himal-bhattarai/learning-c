164b100512aed881dd2b808c7b9309bdbc4446ad
Himal Bhattarai <himalbhattarai439@gmail.com>
2026-04-25 10:04:16 +0545

    day 4:loops

---
+++ loops/for_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    for(int i=1; i<=5; i++){
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
+/*
+How it works:
+    i = 1 → start
+    i <= 5 → condition
+    i++ → update
+*/
