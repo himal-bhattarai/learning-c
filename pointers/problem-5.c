3331ca2664e0461e44fedac3b1e0da70736eb1cb
Himal Bhattarai <himalbhattarai439@gmail.com>
2026-05-15 20:53:08 +0545

    problem-5

---
+++ pointers/problem-5.c
@@ -0,0 +1,22 @@
+// Swap Two Numbers Using Pointers
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int x = 5, y = 10;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
