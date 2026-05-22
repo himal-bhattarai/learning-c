06e88c02b8ebb47a4372dee31511c23e3c794003
Himal Bhattarai <himalbhattarai439@gmail.com>
2026-05-22 18:55:59 +0545

    problem-11

---
+++ pointers/problem-11.c
@@ -0,0 +1,29 @@
+// Dynamic Memory Allocation Using malloc
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", arr + i);
+    }
+
+    printf("Array Elements:\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+
+    free(arr);
+
+    return 0;
+}
