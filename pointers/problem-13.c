70a2ed559914dcf60a8edcc906f194e46482c51e
Himal Bhattarai <himalbhattarai439@gmail.com>
2026-05-25 20:04:15 +0545

    problem-13

---
+++ pointers/problem-13.c
@@ -0,0 +1,15 @@
+// Add Two Numbers Using Pointers
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20;
+    int *p1 = &a;
+    int *p2 = &b;
+
+    int sum = *p1 + *p2;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
